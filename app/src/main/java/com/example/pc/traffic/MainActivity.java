fb355b090192b98651115d9f611ffa7aea51b5bb
foroaomoe <foroaomoe@hotmail.com>
2016-09-28 17:08:36 +0700

    Add

--- app/src/main/java/com/example/pc/traffic/MainActivity.java
+++ app/src/main/java/com/example/pc/traffic/MainActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,7 +30,22 @@ public class MainActivity extends AppCompatActivity {
         ListView myListView = (ListView) findViewById(R.id.listView);
         myListView.setAdapter(objMyAdapter);
 
+        //Activity When Click ListView
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+                myIntenToDetail(i);
+            }
+        });
+
     }   //createListView
+
+    private void myIntenToDetail(int intClick) {
+        Intent objIntent = new Intent(MainActivity.this, ShowDetailActivity.class);
+        objIntent.putExtra("click", intClick);
+        startActivity(objIntent);
+    }
+
 public  void  ClickAboutMe (View view){
         Intent objIntent = new Intent(Intent.ACTION_VIEW);
         objIntent.setData(Uri.parse("http://www.expertlab.ssru.ac.th"));
